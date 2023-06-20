0dede7bbec950a7690bff5951fa1d72b7436ecae
cyntiaaishimwe <ishimwecynthiaa@gmail.com>
2023-06-20 12:22:09 +0300

    assessment

--- app/src/main/java/com/example/mobile_development_assessment/MainActivity2.kt
+++ app/src/main/java/com/example/mobile_development_assessment/MainActivity2.kt
@@ -4,29 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.contact_page.R
+import com.example.contact_page.databinding.ActivityMain2Binding
+import com.example.contact_page.databinding.ActivityMainActivity3loginBinding
 import com.example.contact_page.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+class MainActivity2 : AppCompatActivity() {
+    lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       binding=ActivityMainBinding.inflate(layoutInflater)
+        binding=ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
-}
-      override fun onResume(){
+    }
+    override fun onResume() {
         super.onResume()
         setContentView(binding.root)
-        binding.tvbtn.setOnClickListener {
-            val intent=Intent(this,MainActivity3login::class.java
+        binding.btnbutton.setOnClickListener {
+            val intent = Intent(
+                this, ActivityMainActivity3loginBinding::class.java
             )
             startActivity(intent)
         }
     }
-
-
-
-
-
-
 }
+
--- app/src/main/java/com/example/mobile_development_assessment/MainActivity3login.kt
+++ app/src/main/java/com/example/mobile_development_assessment/MainActivity3login.kt
@@ -4,21 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.contact_page.R
+import com.example.contact_page.databinding.ActivityMain2Binding
+import com.example.contact_page.databinding.ActivityMainActivity3loginBinding
 import com.example.contact_page.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+class MainActivity3login : AppCompatActivity() {
+    lateinit var binding: ActivityMainActivity3loginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       binding=ActivityMainBinding.inflate(layoutInflater)
+        binding=ActivityMainActivity3loginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-}
-      override fun onResume(){
+    }
+    override fun onResume(){
         super.onResume()
         setContentView(binding.root)
-        binding.tvbtn.setOnClickListener {
-            val intent=Intent(this,MainActivity3login::class.java
+        binding.tvButton2.setOnClickListener {
+            val intent=Intent(this,MainActivity::class.java
             )
             startActivity(intent)
         }
