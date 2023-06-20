451a535fbc0137d868423de260da445bb052572a
cyntiaaishimwe <ishimwecynthiaa@gmail.com>
2023-06-20 08:55:16 +0300

    assessment

---
+++ app/src/main/java/com/example/mobile_development_assessment/BillManagement.kt
@@ -0,0 +1,14 @@
+package com.example.mobile_development_assessment
+
+import android.telephony.PhoneNumberUtils
+
+class BillManagement {
+
+//        var tilNames:String,
+//        var etPhoneNumber:String,
+//        var Email:String,
+//        var Password:String,
+
+
+
+}
